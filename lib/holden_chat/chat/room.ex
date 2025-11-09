c35d959dfd5feb1d2febc42ff21669c64ecd5bfc
Rhys Davies <rhys.davies901@gmail.com>
2025-11-08 16:35:29 -0800

    Adding the first few small pieces to a chat room

--- lib/holden_chat/chat/room.ex
+++ lib/holden_chat/chat/room.ex
@@ -7,7 +7,24 @@ defmodule HoldenChat.Chat.Room do
     defaults [:read]
 
     # and a create action, which we'll customize later
-    create :create
+    create :open do
+      accept [:subject]
+    end
+
+    update :close do
+      # We don't want to accept any input here
+      accept []
+
+      validate attribute_does_not_equal(:status, :closed) do
+        message "Ticket is already closed"
+      end
+
+      change set_attribute(:status, :closed)
+      # A custom change could be added like so:
+      #
+      # change MyCustomChange
+      # change {MyCustomChange, opt: :val}
+    end
   end
 
   # Attributes are the simple pieces of data that exist on your resource
@@ -16,6 +33,28 @@ defmodule HoldenChat.Chat.Room do
     uuid_primary_key :id
 
     # Add a string type attribute called `:subject`
-    attribute :subject, :string
+    attribute :subject, :string do
+      # Don't allow `nil` values
+      allow_nil? false
+
+      # Allow this attribute to be public. By default, all attributes are private.
+      public? true
+    end
+
+    # status is either `open` or `closed`. We can add more statuses later
+    attribute :status, :atom do
+      # Constraints allow you to provide extra rules for the value.
+      # The available constraints depend on the type
+      # See the documentation for each type to know what constraints are available
+      # Since atoms are generally only used when we know all of the values
+      # it provides a `one_of` constraint, that only allows those values
+      constraints one_of: [:open, :closed]
+
+      # The status defaulting to open makes sense
+      default :open
+
+      # We also don't want status to ever be `nil`
+      allow_nil? false
+    end
   end
 end
